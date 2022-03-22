68ac17cf5486827194fac4397764fb6f415f3184
Robi MS <102130989+robimasptr@users.noreply.github.com>
2022-03-22 13:59:29 +0700

    Add files via upload

---
+++ keripik.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Mar 2022 pada 08.37
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `keripik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` varchar(100) NOT NULL,
+  `id_barang` varchar(100) NOT NULL,
+  `jumlahtersedia` int(11) NOT NULL,
+  `jumlahterjual` int(11) NOT NULL,
+  `stokakhir` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `id_barang`, `jumlahtersedia`, `jumlahterjual`, `stokakhir`) VALUES
+('1', '1', 3, 3, 0),
+('2', '1', 6, 6, 0),
+('3', '3', 7, 7, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblbarang`
+--
+
+CREATE TABLE `tblbarang` (
+  `id_barang` varchar(100) NOT NULL,
+  `jeniskeripik` varchar(100) NOT NULL,
+  `rasakeripik` varchar(100) NOT NULL,
+  `harga` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tblbarang`
+--
+
+INSERT INTO `tblbarang` (`id_barang`, `jeniskeripik`, `rasakeripik`, `harga`) VALUES
+('1', 'Kentang', 'Asin', 3000),
+('2', 'Bayam', 'Asin', 4000),
+('3', 'Singkong', 'Asin', 5000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indeks untuk tabel `tblbarang`
+--
+ALTER TABLE `tblbarang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD CONSTRAINT `penjualan_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tblbarang` (`id_barang`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
